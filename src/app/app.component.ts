87d89e0f75740c292e2fff5005b239f37ffc6dff
Sathiyadev N <sathiyadevnatarajan43@gmail.com>
2022-10-12 23:31:30 +0530

    Text Interpolation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'movies-app';
+  title = 'movies-app --> Title of App Component';
+
+  today=new Date();
+
+  
 }
